e357d72732673a07a1b9961be36f57556a37edba
Lioncat2002 <sankhayan2002@gmail.com>
2021-07-04 11:15:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ async def root():
   r = d.decode('utf-8').split('\n')
   facts = requests.get('https://raw.githubusercontent.com/AceKiron/public_data/main/axolotl_facts.txt').content
   fact_list = facts.decode('utf-8').split('\n')
-  return JSONResponse({"url":random.choice(r),"facts":random.choice(fact_list),"pics_repo":"https://github.com/AceKiron/public_data"})
+  return JSONResponse({"url":random.choice(r),
+                       "facts":random.choice(fact_list),
+                       "pics_repo":"https://github.com/AceKiron/public_data",
+                       "api_repo":"https://github.com/Lioncat2002/axolotl.py-api"})
 
 #keep_alive()
