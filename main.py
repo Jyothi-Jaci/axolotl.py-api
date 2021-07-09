ae2c0f91847b90ff9334457a7468096c40cc047f
Lioncat2002 <sankhayan2002@gmail.com>
2021-07-09 20:35:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ api=FastAPI()
 
 @api.get("/")
 async def root():
-  d = requests.get('https://raw.githubusercontent.com/AceKiron/public_data/main/axolotl_pictures.txt').content
+  d = requests.get('https://raw.githubusercontent.com/AxolotlAPI/data/main/pictures.txt').content
   r = d.decode('utf-8').split('\n')
-  facts = requests.get('https://raw.githubusercontent.com/AceKiron/public_data/main/axolotl_facts.txt').content
+  facts = requests.get('https://github.com/AxolotlAPI/data/blob/main/facts.txt').content
   fact_list = facts.decode('utf-8').split('\n')
   return JSONResponse({"url":random.choice(r),
                        "facts":random.choice(fact_list),
-                       "pics_repo":"https://github.com/AceKiron/public_data",
-                       "api_repo":"https://github.com/Lioncat2002/axolotl.py-api"})
+                       "pics_repo":"https://github.com/AxolotlAPI/data",
+                       "api_repo":"https://github.com/AxolotlAPI/axolotl.py-api"})
 
 #keep_alive()
